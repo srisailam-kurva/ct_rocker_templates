ccb2de569adc8315ae1d59ce74d8c4e2585cdee1
srisailam-kurva <srisailam.kurva@cloudtern.com>
2024-01-23 12:15:42 +0530

    new code

---
+++ rockerfw/src/main/java/com/rocker/RockerfwApplication.java
@@ -0,0 +1,13 @@
+package com.rocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockerfwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockerfwApplication.class, args);
+	}
+
+}
---
+++ rockerfw/src/test/java/com/rocker/RockerfwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockerfwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
